4a3b6edde9cdf377a95b814e14915e90dea1f44c
SofaDu <115635511+SofaDu@users.noreply.github.com>
2022-11-21 15:40:54 +0300

    Carrrrrrrrrrrr

---
+++ Car/Car/Car.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Car
+{
+    public class Car
+    {
+        int doors, horsePower;
+        string color, mark, vinNumber;
+        public int Doors
+        {
+            get => doors;
+            set
+            {
+                this.doors = value;
+            }
+        }
+        public int HorsePower
+        {
+            get => horsePower;
+            set
+            {
+                this.horsePower = value;
+            }
+        }
+        public string Color
+        {
+            get => color;
+            set
+            {
+                this.color = value;
+            }
+        }
+        public string Mark
+        {
+            get => mark;
+            set
+            {
+                this.mark = value;
+            }
+        }
+        public string VinNumber
+        {
+            get => vinNumber;
+            set
+            {
+                this.vinNumber = value;
+            }
+        }
+        public override string ToString() => $"Mark: {Mark}\n" +
+            $"VIN-Number: {VinNumber}\n" +
+            $"Color: {Color}\n" +
+            $"Count of doors: {Doors}\n" +
+            $"Horse power: {HorsePower}\n";
+    }
+
+    public class Sedan : Car
+    {
+        bool presenceOfSpareWheel;
+        public bool PresenceOfSpareWheel
+        {
+            get => presenceOfSpareWheel;
+            set
+            {
+                this.presenceOfSpareWheel = value;
+            }
+        }
+        public override string ToString() => base.ToString() + 
+            $"The presence of a spare wheel: {PresenceOfSpareWheel}\n";
+    }
+    public class Hatchback : Car
+    {
+        int trunkVolume;
+        public int TrunkVolume
+        {
+            get => trunkVolume;
+            set
+            {
+                this.trunkVolume = value;
+            }
+        }
+        public override string ToString() => base.ToString() +
+            $"The trunk volume: {TrunkVolume}\n";
+    }
+    public class Liftback : Car 
+    {
+        double timeTo100;
+        public double TimeTo100
+        {
+            get => timeTo100;
+            set
+            {
+                this.timeTo100 = value;
+            }
+        }
+        public override string ToString() => base.ToString() +
+            $"Time to 100: {TimeTo100}\n";
+    }
+}
---
+++ Car/Car/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Car
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Liftback S7 = new Liftback();
+            S7.Mark = "Audi S7";
+            S7.VinNumber = "12986498124";
+            S7.Color = "White";
+            S7.Doors = 5;
+            S7.HorsePower = 450;
+            S7.TimeTo100 = 4.6;
+
+            Console.WriteLine(S7);
+        }
+    }
+}
